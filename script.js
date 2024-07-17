c0f9cbe832a571e362ce000dee8e7b48628be28f
nadeemk2023 <nadeemekhan@gmail.com>
2024-07-16 18:43:51 -0700

    chore: Add showRating function to display star ratings
    My attempt added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('testing link between html and js');
+/* #1. Show Rating
+Given a rating, display a star (*) for each whole number, and a full stop (.) for each half rating.
+
+Example:
+showRating(3); // returns '***'
+showRating(4.5); // returns '****.'
+showRating(0.5); // returns '.' 
+
+*/
+
+function showRating(rating) {
+  if (Number.isInteger(rating)) {
+    return '*'.repeat(rating);
+  } else {
+    return '*'.repeat(rating) + '.';
+  }
+}
+
+console.log(showRating(3.5));
